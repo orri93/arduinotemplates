a39a0ecf1324642a7f77835f995bd03652988d10
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2019-11-18 22:44:13 +0100

    Add elemental type of buffer to the template and improve the EEPROM support

--- src/gatlbuffer.h
+++ src/gatlbuffer.h
@@ -3,20 +3,22 @@
 
 #include <Arduino.h>
 
+#include <gatlbinding.h>
+
 #ifndef GOS_ARDUINO_TEMPLATE_LIBRARY_BUFFER_SIZE
 #define GOS_ARDUINO_TEMPLATE_LIBRARY_BUFFER_SIZE           11
 #endif
 
 namespace gos {
 namespace atl {
-
 namespace buffer {
-template<typename S = uint8_t> class Holder {
+
+template<typename S = uint8_t, typename C = char> class Holder {
 public:
   Holder(const S& size = GOS_ARDUINO_TEMPLATE_LIBRARY_BUFFER_SIZE) : Size(size) {
-    Buffer = (char*)(malloc(size));
+    Buffer = (C*)(malloc(size));
   }
-  Holder(const char* literal, const S& size) : Buffer(const_cast<char*>(literal)), Size(size - 1) {
+  Holder(const C* literal, const S& size) : Buffer(const_cast<C*>(literal)), Size(size - 1) {
   }
 #ifdef GOS_ARDUINO_TEMPLATE_LIBRARY_CLEANUP
   void cleanup() {
@@ -27,11 +29,51 @@ public:
     }
   }
 #endif
-  char* Buffer;
+  C* Buffer;
   S Size;
 };
+
+template<typename T, typename S = uint8_t, typename C = uint8_t>
+S write(Holder<S>& buffer, const S& size, const S& index, T* pointer) {
+  C* cp = (C*)(pointer);
+  for (S i = 0; i < size; i++) {
+    buffer.Buffer[index++] = *(cp++);
+  }
+  return index;
 }
 
+template<typename T, typename S = uint8_t, typename C = uint8_t>
+S read(const Holder<S>& buffer, const S& size, const S& index, T* pointer) {
+  C* cp = (C*)(pointer);
+  for (S i = 0; i < size; i++) {
+    *(cp++) = buffer.Buffer[index++];
+  }
+  return index;
+}
+
+template<typename T, typename S = uint8_t, typename C = uint8_t>
+S write(
+  Holder<S>& buffer,
+  ::gos::atl::binding::reference<T, S, C>& binding) {
+  S index = binding.first;
+  for (uint8_t i = 0; i < binding.count; i++) {
+    index = write<T, S, C>(buffer, binding.size, index, binding.pointers[i]);
+  }
+  return index;
+}
+
+template<typename T, typename S = uint8_t, typename C = uint8_t>
+S read(
+  const Holder<S>& buffer,
+  ::gos::atl::binding::reference<T, S, C>& binding) {
+  S index = binding.first;
+  for (uint8_t i = 0; i < binding.count; i++) {
+    index = write<T, S, C>(buffer, binding.size, index, binding.pointers[i]);
+  }
+  return index;
+}
+
+}
 }
 }
 
--- src/gatleeprom.h
+++ src/gatleeprom.h
@@ -1,14 +1,17 @@
 #ifndef _GOS_ARDUINO_TEMPLATE_LIBRARY_EEPROM_H_
 #define _GOS_ARDUINO_TEMPLATE_LIBRARY_EEPROM_H_
 
-#define USE_AVR_EEPROM
+#define GOS_ATL_USE_AVR_EEPROM
+#define GOS_ATL_USE_ARDUINO_EEPROM
 
-#ifdef USE_AVR_EEPROM
+#ifdef GOS_ATL_USE_AVR_EEPROM
 #include <avr/eeprom.h>
-#else
+#endif
+#ifdef GOS_ATL_USE_ARDUINO_EEPROM
 #include <EEPROM.h>
 #endif
 
+#include <gatlbuffer.h>
 #include <gatlbinding.h>
 #include <gatlutility.h>
 
@@ -20,27 +23,77 @@ namespace gos {
 namespace atl {
 namespace eeprom {
 
-template<typename T>
-int write(const uint8_t& size, int index, T* pointer) {
+template<typename T, typename A>
+A write(const uint8_t& size, A address, T* pointer) {
   uint8_t* cp = (uint8_t*)(pointer);
   for (uint8_t i = 0; i < size; i++) {
-    EEPROM.write(index++, *(cp++));
+#ifdef GOS_ATL_USE_AVR_EEPROM
+    eeprom_write_byte(reinterpret_cast<uint8_t*>(address++), *(cp++));
+#endif
+#ifdef GOS_ATL_USE_ARDUINO_EEPROM
+    EEPROM.write(address++, *(cp++));
+#endif
   }
-  return index;
+  return address;
 }
 
+template<typename T, typename A>
+A update(const uint8_t& size, A address, T* pointer) {
+  uint8_t* cp = (uint8_t*)(pointer);
+  for (uint8_t i = 0; i < size; i++) {
+#ifdef GOS_ATL_USE_AVR_EEPROM
+    eeprom_update_byte((uint8_t*)address++, *(cp++));
+#endif
+#ifdef GOS_ATL_USE_ARDUINO_EEPROM
+    EEPROM.update(address++, *(cp++));
+#endif
+  }
+  return address;
+}
 
 template<typename T>
 int read(const uint8_t& size, int index, T* pointer) {
   uint8_t* cp = (uint8_t*)(pointer);
   for (uint8_t i = 0; i < size; i++) {
-#ifdef USE_AVR_EEPROM
-    * (cp++) = eeprom_read_byte(index++);
-#else
+#ifdef GOS_ATL_USE_AVR_EEPROM
+    * (cp++) = eeprom_read_byte(reinterpret_cast<const uint8_t*>( index++));
+#endif
+#ifdef GOS_ATL_USE_ARDUINO_EEPROM
     *(cp++) = EEPROM.read(index++);
 #endif
   }
   return index;
+}
+
+template<typename S = uint8_t, typename A = uint16_t>
+int read(
+  ::gos::atl::buffer::Holder<S>& buffer,
+  const A& address,
+  const S& size) {
+  ::eeprom_read_block(
+    (void*)(buffer.Buffer),
+    (const void*)address,
+    (size_t)size);
+}
+template<typename S = uint8_t, typename A = uint16_t>
+int update(
+  const ::gos::atl::buffer::Holder<S>& buffer,
+  const A& address,
+  const S& size) {
+  ::eeprom_update_block(
+    (const void*)(buffer.Buffer),
+    (void*)address,
+    (size_t)size);
+}
+template<typename S = uint8_t, typename A = uint16_t>
+int write(
+  const ::gos::atl::buffer::Holder<S>& buffer,
+  const A& address,
+  const S& size) {
+  ::eeprom_write_block(
+    (const void*)(buffer.Buffer),
+    (void*)address,
+    (size_t)size);
 }
 
 template<typename T> void write(
